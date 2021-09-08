d4bba3834b322e9df36a0aaf14930815476ad1b2
Rigel <43711248+rigelselmani@users.noreply.github.com>
2021-09-07 23:48:54 -0400

    generating the colors and displaying to the squares

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-var square=document.getElementsByClassName("square")
+var num=6;
+var squares=document.querySelectorAll(".square");
 var arr=[]
-for (var i=0;i<square;i++){
+
+
+for (var i=0;i<squares.length;i++){
+     squares[i].style.background=`${arr[i]}`
+}
+
+for (var i=0;i<num;i++){
     arr.push(randomColor())
 }
 
