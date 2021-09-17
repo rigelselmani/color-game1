2773751332275de974d412ae7a20f76d5b396043
Rigel <43711248+rigelselmani@users.noreply.github.com>
2021-09-16 22:27:42 -0400

    click event

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-var numSquares =6;
-var colors=generateRandomColors(numSquares);
+var colors=generateRandomColors();
 
 var squares=document.querySelectorAll(".square");
 
 
 
     for (var i=0;i<squares.length;i++){
-            squares[i].style.background=colors[i]
+        squares[i].style.background=colors[i]
+
+        squares[i].addEventListener("click", function(data){
+                 console.log(data)
+        this.squares[i].style.background=colors[i]
+        })
+        squares[i].style.background=colors[i]
+            
    }
 
-function generateRandomColors(num){
+
+function generateRandomColors(){
     var arr=[]
-    for (var i=0;i<num;i++){
+    for (var i=0;i<6;i++){
         arr.push(randomColor())
     }
     return arr
