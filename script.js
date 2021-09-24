307a93543bcd4575e51687a972df3f17422f89eb
Rigel <43711248+rigelselmani@users.noreply.github.com>
2021-09-23 23:23:28 -0400

    working on if you click the rong or the right color

--- script.js
+++ script.js
@@ -11,12 +11,44 @@ for (var i=0;i<squares.length;i++){
     if(clicked.length==0){
             clicked.push(this.style.background)
             firstClick();
-            console.log(clicked.length)
+            console.log(clicked)
         }
     else if(clicked.length==1){
+        if(clicked==this.style.background){
+            console.log("you got it")
+          
+        }
+            clicked.push(this.style.background)
             secondClick();
             console.log(clicked)
         }
+    else if(clicked.length==2){
+        if(clicked==this.style.background){
+            console.log("you got it")
+            
+        }
+            clicked.push(this.style.background)
+            thirdClick();
+            console.log(clicked)
+        }
+    else if(clicked.length==3){
+        if(clicked==this.style.background){
+            console.log("you got it")
+            
+        }
+            clicked.push(this.style.background)
+            fourthClick();
+            console.log(clicked)
+    }
+    else if(clicked.length==4){
+        if(clicked==this.style.background){
+            console.log("you got it")
+           
+        }
+            clicked.push(this.style.background)
+            fifthClick();
+            console.log(clicked)
+}
     })
 }
 
@@ -31,6 +63,25 @@ function firstClick(){
 
 function secondClick(){
     var array=["green","blue","yellow","black","white","red"];
+    for (var i=0;i<squares.length;i++){
+        squares[i].style.background=array[i]
+    }
+}
+
+function thirdClick(){
+    var array=["yellow","red","white","black","blue","green"]
+    for (var i=0;i<squares.length;i++){
+        squares[i].style.background=array[i]
+    }
+}
+function fourthClick(){
+    var array=["black","blue","yellow","red","green","white"]
+    for (var i=0;i<squares.length;i++){
+        squares[i].style.background=array[i]
+    }
+}
+function fifthClick(){
+    var array=["white","yellow","black","green","blue","red"]
     for (var i=0;i<squares.length;i++){
         squares[i].style.background=array[i]
     }
